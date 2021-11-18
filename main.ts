3518a61b9d5a6a8433ca7ebc383504d71df58bd7
Oliver Joseph Ash <oliverjash@gmail.com>
2021-11-18 12:30:01 +0000

    Init

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 import { createApi } from 'unsplash-js';
 import * as nodeFetch from 'node-fetch'
 
+declare global {
+  var fetch: typeof nodeFetch.default;
+  type RequestInit = nodeFetch.RequestInit;
+  type Response = nodeFetch.Response;
+}
+global.fetch = nodeFetch.default;
+
 const unsplash = createApi({
   accessKey: 'MY_ACCESS_KEY',
-  fetch: nodeFetch.default as unknown as typeof fetch,
+  fetch: nodeFetch.default,
 });
